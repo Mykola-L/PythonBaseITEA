bf607670e82e1bc7c1ceff7e408e4d28ad130f4e
Mykola Lavrushko <mykolalavrushko@gmail.com>
2020-04-30 20:47:01 +0300

    Fixed formula for calculating cicle area

--- Lesson1/Task2.py
+++ Lesson1/Task2.py
@@ -1,5 +1,5 @@
-circleRadius = input('Введіть, будь-ласка, радіус кола')
+circleRadius = input('Введіть, будь-ласка, радіус кола ')
 circleRadius = int(circleRadius)
 p=3.14159
-areaCircle = p * circleRadius
+areaCircle = p * circleRadius ** 2
 print('Площа кола', areaCircle, 'см.')
