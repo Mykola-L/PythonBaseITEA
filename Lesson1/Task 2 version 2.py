12c0aba6dda2e78830a618e8cb05f0e806e6609d
Mykola Lavrushko <mykolalavrushko@gmail.com>
2020-05-02 00:27:54 +0300

    added a number pi

---
+++ Lesson1/Task 2 version 2.py
@@ -0,0 +1,6 @@
+import math
+#print (math.pi)
+circleRadius = input('Введіть, будь-ласка, радіус кола ')
+circleRadius = int(circleRadius)
+areaCircle = math.pi * circleRadius ** 2
+print('Площа кола', areaCircle, 'см.')
