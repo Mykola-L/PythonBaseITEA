c8d87d25f453f7f3c4f85545c25ec8e47ca99723
Mykola Lavrushko <mykolalavrushko@gmail.com>
2020-05-04 15:53:05 +0300

    corrected Task4, Lesson 2

--- Lesson2/Task4.py
+++ Lesson2/Task4.py
@@ -4,9 +4,9 @@ unit = input('Введіть, будь-ласка, одиницю виміру (
 if unit == 'mm':
     print(f'Значення в міліметрах {number * 1000}')
 elif unit == 'sm':
-        print(f'Значення в сантиметрах {number * 100}')
+    print(f'Значення в сантиметрах {number * 100}')
 elif unit == 'km':
-        print(f'Значення в кілометрах {number / 1000}')
+    print(f'Значення в кілометрах {number / 1000}')
 else:
     print('Ви ввели невірні дані')
 
